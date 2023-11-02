0450c754fb62324c2130558546dfdbaa2bfc55ff
halfrice <halfrice@protonmail.com>
2023-11-01 17:11:38 -0700

    Add projects page

---
+++ app/(routes)/projects/page.tsx
@@ -0,0 +1,22 @@
+const ProjectsPage = () => {
+  return (
+    <section className="flex flex-col items-center justify-between w-full min-w-full">
+      <div className="flex flex-col items-center lg:items-start w-full max-w-5xl mt-20 px-4 lg:px-0">
+        <h1 className="text-2xl lg:text-3xl text-[--green]">Projects</h1>
+        <div className="text-sm lg:text-base">
+          <p className="mt-8">
+            Ullamco exercitation aliqua enim minim quis aliqua id commodo. Minim
+            id officia amet excepteur. Minim commodo nostrud ipsum elit minim
+            duis elit exercitation Lorem excepteur magna sit. Voluptate
+            excepteur occaecat amet eiusmod elit. Elit elit ea adipisicing ex
+            ut. Quis fugiat dolore sint aliquip fugiat officia dolor minim
+            aliqua aliquip deserunt minim culpa tempor. Qui aliqua est labore
+            cillum.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsPage
