7efc827405657a561a6ed8e05328eaf1c2466f15
halfrice <halfrice@protonmail.com>
2023-11-01 17:09:15 -0700

    Add contact page

---
+++ app/(routes)/contact/page.tsx
@@ -0,0 +1,18 @@
+const ContactPage = () => {
+  return (
+    <section className="flex flex-col items-center justify-between w-full min-w-full">
+      <div className="flex flex-col items-center lg:items-start w-full max-w-5xl mt-20 px-4 lg:px-0">
+        <h1 className="text-2xl lg:text-3xl text-[--purple]">Contact</h1>
+        <div className="text-sm lg:text-base">
+          <p className="mt-8">
+            Nulla ut est eu dolor occaecat tempor. Adipisicing Lorem proident
+            voluptate aute adipisicing aliquip laboris. Aliqua pariatur tempor
+            nostrud tempor minim.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ContactPage
